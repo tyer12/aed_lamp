63ce0aba5c4984fed66e8e117584c8641f4d4ed0
André Sabino <705800+amgs@users.noreply.github.com>
2020-03-18 12:08:11 +0000

    Add failing set_off test.

--- test_controllers.py
+++ test_controllers.py
@@ -6,6 +6,11 @@ class TestLampController(unittest.TestCase):
         controller = LampController()
         controller.set_on()
         self.assertTrue(controller.get_status())
+    
+    def test_test_off(self):
+        controller = LampController()
+        controller.set_off()
+        self.assertFalse(controller.get_status())
 
 if __name__ == "__main__":
     unittest.main()
